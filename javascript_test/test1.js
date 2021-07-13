a76ce270004404793677584951a730903ecfbae9
DESKTOP-NUOPUIF\ITPS <heansum1556@gmail.com>
2021-07-13 17:57:56 +0900

    first commit

---
+++ javascript_test/test1.js
@@ -0,0 +1,16 @@
+// 자바스크립트 = 인터프리터 언어
+// 타입추론
+
+let int_num = 1;
+let double_num = 1.0;
+let bool_data = true;
+let string_data = "안녕";
+let char_data = "가";
+let back_data = `백틱 ${int_num}`;
+let list_data = [1,2,3,4];
+let object_data = {
+    id: 1,
+    username: "ssar"
+};
+
+console.log(int_num);
